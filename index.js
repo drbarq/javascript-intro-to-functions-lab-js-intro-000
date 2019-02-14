b2426a8a16f05082c3e5f9da10945d0fb8529671
Joe Tustin <j.tustin@gmail.com>
2019-02-14 20:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
+  if (string === "I Love you, Grandma.") {
+    return "I love you, too."
+  }
 }
