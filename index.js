4a7e1b4377b9d430ad2cb21da2e022bb880647b6
Joe Tustin <j.tustin@gmail.com>
2019-02-14 20:07:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function shout(string) {
   return string.toUpperCase()
-  console.log(shout.toUpperCase)
 }
 
 function whisper(string) {
   return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log.toUpperCase()
 }
